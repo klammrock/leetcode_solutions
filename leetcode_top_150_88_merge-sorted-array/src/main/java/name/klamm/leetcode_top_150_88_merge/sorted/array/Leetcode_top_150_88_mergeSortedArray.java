280a96c59cdb3071b4b72691b9eaede3874048fe
Valery Baranov <v.baranov@integer-soft.ru>
2024-01-15 01:02:58 +0400

    resolve leetcode_top_150_88_merge-sorted-array

--- leetcode_top_150_88_merge-sorted-array/src/main/java/name/klamm/leetcode_top_150_88_merge/sorted/array/Leetcode_top_150_88_mergeSortedArray.java
+++ leetcode_top_150_88_merge-sorted-array/src/main/java/name/klamm/leetcode_top_150_88_merge/sorted/array/Leetcode_top_150_88_mergeSortedArray.java
@@ -2,24 +2,20 @@ package name.klamm.leetcode_top_150_88_merge.sorted.array;
 
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int i = m - 1, j = m + n - 1; i >= 0; --i, --j ) {
-            nums1[j] = nums1[i];
-        }
+        int nums1Pos = m - 1;
+        int nums2Pos = n - 1;
+        int nums1InsertPos = m + n - 1;
 
-        int nums1Pos = n;
-        int nums2Pos = 0;
-        int nums1InsertPos = 0;
-
-        while (nums1InsertPos < m + n) {
-            int num1 = (nums1Pos < m + n) ? nums1[nums1Pos] : Integer.MAX_VALUE;
-            int num2 = (nums2Pos < n) ? nums2[nums2Pos] : Integer.MAX_VALUE;
+        while (nums1InsertPos >= 0) {
+            int num1 = (nums1Pos >= 0) ? nums1[nums1Pos] : Integer.MIN_VALUE;
+            int num2 = (nums2Pos >= 0) ? nums2[nums2Pos] : Integer.MIN_VALUE;
 
             if (num1 > num2) {
-                nums1[nums1InsertPos++] = num2;
-                ++nums2Pos;
+                nums1[nums1InsertPos--] = num1;
+                --nums1Pos;
             } else {
-                nums1[nums1InsertPos++] = num1;
-                ++nums1Pos;
+                nums1[nums1InsertPos--] = num2;
+                --nums2Pos;
             }
         }
     }
